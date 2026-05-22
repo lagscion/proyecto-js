e9a9c11a19bdc1e599bb1f6c5dcdc1a1ee7a5ebe
lagscion <>
2026-05-22 16:55:27 -0500

    se agrega el loggin y algunos estilos

---
+++ js/auth.js
@@ -0,0 +1,24 @@
+    export function ingresar() {
+        const usuario = document.getElementById("usuario");
+        const pswrd = document.getElementById("contrasena");
+
+        const usu = usuario.value;
+        const contra = pswrd.value;
+
+        if (contra !== "123456" || usu !== "admin@mail.com") {
+            console.log("Usuario o contraseña incorrectos");
+            alert("Acceso denegado");
+        } else {
+                console.log("¡Acceso correcto!");
+                alert("Bienvenido");
+            setTimeout(() => {
+                window.location.href = "admin.html";
+            }, 500);
+        }
+    }
+
+    const boton = document.getElementById("btnIngresar");
+    boton.addEventListener("click", ingresar);
+
+
+
