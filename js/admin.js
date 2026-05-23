f20b70ce9e95806b3c3a0358325ecdb51de8bba3
lagscion <76491526+lagscion@users.noreply.github.com>
2026-05-23 16:20:03 -0500

    feature(admin): se agrega capa de seguridad

---
+++ js/admin.js
@@ -0,0 +1,9 @@
+function sesion (){
+    let ini = localStorage.getItem("sesion")
+    let sesion = JSON.parse(ini)
+    if (!sesion){
+        window.location.href = "../pages/loggin.html"
+    }
+}
+
+sesion();
