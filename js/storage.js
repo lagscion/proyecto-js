fc88cf094dd1ad670459fdff9ae6ddd0bd0259ce
lagscion <76491526+lagscion@users.noreply.github.com>
2026-05-23 14:34:35 -0500

    feature(all)termino las funciones del storage

---
+++ js/storage.js
@@ -0,0 +1,33 @@
+export function obtenerTodos(clave){
+    const datos = localStorage.getItem(clave);
+    if (!datos) {
+        return [];
+    }
+    return JSON.parse(datos);
+}
+
+        export function agrergarItem(clave, valor){
+            const array = obtenerTodos(clave)
+            valor.id = Date.now()
+            array.push (valor)
+
+            localStorage.setItem(clave, JSON.stringify(array) )
+        }
+
+        export function actualizarItem(clave, id, datosNuevos){
+            const datos = obtenerTodos(clave)
+            let arrayact = datos.map(item => {
+            if(item.id === id){
+                return {...item, ...datosNuevos}
+            }
+            return item;
+            }
+        )
+        localStorage.setItem(clave, JSON.stringify(arrayact));
+        }
+
+        export function eliminarItem(clave, id ){
+            const datos = obtenerTodos(clave);
+            let newarray = datos.filter(item => item.id !== id)
+            localStorage.setItem(clave, JSON.stringify(newarray));
+        }
