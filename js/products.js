ca6b3b751973d1277742e81f37a64fad327b00a5
lagscion <76491526+lagscion@users.noreply.github.com>
2026-05-24 10:49:11 -0500

    feature(products.js): se agregan las funciones que hacen que se creen los productos con sus respondientes opciones

---
+++ js/products.js
@@ -0,0 +1,132 @@
+import {
+  obtenerTodos,
+  agregarItem,
+  actualizarItem,
+  eliminarItem,
+} from "./storage.js";
+
+let ideditando = null;
+
+function mostrarProductos() {
+  const productos = obtenerTodos("productos");
+  const contenedor = document.getElementById("productos");
+
+  contenedor.innerHTML = productos
+    .map((producto) => {
+      return `
+        <div>
+            <h1>${producto.nombre}</h1>
+            <p>${producto.codigo}</p>
+            <p>${producto.precio}</p>
+            <img src="${producto.imagen}">
+            <p>${producto.descripcion}</p>
+            <button data-id="${producto.id}">Editar</button>
+            <button data-id="${producto.id}">Eliminar</button>
+        </div>
+    `;
+    })
+    .join("");
+}
+
+function agregarProducto() {
+  if (ideditando === null) {
+    // declaro variables
+    const codigo = document.getElementById("codigo").value;
+    const nom = document.getElementById("nombre").value;
+    const categoria = document.getElementById("categoria").value;
+    const precio = document.getElementById("precio").value;
+    const imagen = document.getElementById("imagen").value;
+    const descrp = document.getElementById("descripcion").value;
+    //declaro variables
+
+    agregarItem("productos", {
+      codigo,
+      nom,
+      categoria,
+      precio,
+      imagen,
+      descrp,
+    });
+    document.getElementById("codigo").value = "";
+    document.getElementById("nombre").value = "";
+    document.getElementById("categoria").value = "";
+    document.getElementById("precio").value = "";
+    document.getElementById("imagen").value = "";
+    document.getElementById("descripcion").value = "";
+    cerrarModal();
+    mostrarProductos();
+  } else {
+    const codigo = document.getElementById("codigo").value;
+    const nom = document.getElementById("nombre").value;
+    const categoria = document.getElementById("categoria").value;
+    const precio = document.getElementById("precio").value;
+    const imagen = document.getElementById("imagen").value;
+    const descrp = document.getElementById("descripcion").value;
+    actualizarItem("productos", ideditando, {
+      codigo,
+      nom,
+      categoria,
+      precio,
+      imagen,
+      descrp,
+    });
+    ideditando = null;
+    cerrarModal();
+    mostrarProductos();
+  }
+}
+
+function abrirModal() {
+  document.getElementById("codigo").value = "";
+  document.getElementById("nombre").value = "";
+  document.getElementById("categoria").value = "";
+  document.getElementById("precio").value = "";
+  document.getElementById("imagen").value = "";
+  document.getElementById("descripcion").value = "";
+  document.getElementById("modal").style.visibility = "visible";
+}
+
+    function cerrarModal() {
+    let cont = document.getElementById("modal");
+    cont.style.visibility = "hidden";
+    }
+
+    document.addEventListener("DOMContentLoaded", function () {
+        mostrarProductos();
+    
+        const agregar = document.getElementById("agregar");
+        agregar.addEventListener("click", abrirModal);
+        const guardar = document.getElementById("guardar");
+        guardar.addEventListener("click", agregarProducto);
+    
+        document
+          .getElementById("productos")
+          .addEventListener("click", function (e) {
+            if (e.target.textContent === "Eliminar") {
+              const id = e.target.dataset.id;
+              eliminarItem("productos", id);
+              mostrarProductos();
+            }
+            if (e.target.textContent === "Editar") {
+              const id = e.target.dataset.id;
+              editar(id);
+            }
+          });
+        
+    });
+    
+
+    function editar(id){
+      ideditando = Number(id)
+      let array = obtenerTodos("productos")
+      let Aeditar =array.find((item) => item.id === ideditando);
+  document.getElementById("codigo").value = Aeditar.codigo ;
+  document.getElementById("nombre").value = Aeditar.nombre ;
+  document.getElementById("categoria").value = Aeditar.categoria;
+  document.getElementById("precio").value = Aeditar.precio ;
+  document.getElementById("imagen").value = Aeditar.imagen ;
+  document.getElementById("descripcion").value = Aeditar.descrp;
+      abrirModal();
+    }
+    
+    
