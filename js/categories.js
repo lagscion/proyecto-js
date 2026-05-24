0a00f87d0661991aa4442d12196f3428cbac1341
lagscion <76491526+lagscion@users.noreply.github.com>
2026-05-23 20:51:43 -0500

    feature(categoria.js):se agrega a el codigo la funcionalidad del boton de editar

--- js/categories.js
+++ js/categories.js
@@ -5,6 +5,8 @@ import {
   eliminarItem,
 } from "./storage.js";
 
+let ideditando = null
+
 function mostrarCategorias(){
     const categorias = obtenerTodos("categorias")
     const contenedor = document.getElementById("tarjetas")
@@ -23,15 +25,25 @@ contenedor.innerHTML = categorias.map((categoria) => {
 }
 
 function agregarCategoria (){
-    let x = document.getElementById("nombre") 
-    const nombre = x.value
-    let y = document.getElementById("descripcion");
-    const descripcion = y.value
-    agregarItem("categorias", {nombre, descripcion })
-    x.value = ""
-    y.value = ""
-    cerrarModal();
-    mostrarCategorias();
+    if (ideditando === null ){
+      let x = document.getElementById("nombre");
+      const nombre = x.value;
+      let y = document.getElementById("descripcion");
+      const descripcion = y.value;
+      agregarItem("categorias", { nombre, descripcion });
+      x.value = "";
+      y.value = "";
+      cerrarModal();
+      mostrarCategorias();
+    }
+    else{
+          const nombre = document.getElementById("nombre").value;
+          const descripcion = document.getElementById("descripcion").value;
+          actualizarItem("categorias", ideditando, { nombre, descripcion });
+          ideditando = null;
+          cerrarModal();
+          mostrarCategorias();
+    }
 }
 
 function abrirModal(){
@@ -46,16 +58,33 @@ function cerrarModal() {
 
 document.addEventListener("DOMContentLoaded", function () {
     mostrarCategorias();
+
     const agregar = document.getElementById("btnAgregar");
     agregar.addEventListener("click", abrirModal);
     const guardar = document.getElementById("btnGuardar");
     guardar.addEventListener("click", agregarCategoria);
+
     document.getElementById("tarjetas").addEventListener("click", function (e) {
       if (e.target.textContent === "Eliminar") {
         const id = e.target.dataset.id;
         eliminarItem("categorias", id);
         mostrarCategorias()
       }
+      if (e.target.textContent === "Editar"){
+        const id = e.target.dataset.id;
+        editar(id);
+        
+      }
     });
+    
 });
+
+function editar(id){
+  ideditando = Number(id)
+  let array = obtenerTodos("categorias")
+  let Aeditar =array.find((item) => item.id === ideditando);
+  document.getElementById("nombre").value = Aeditar.nombre;
+  document.getElementById("descripcion").value = Aeditar.descripcion;
+  abrirModal();
+}
 
